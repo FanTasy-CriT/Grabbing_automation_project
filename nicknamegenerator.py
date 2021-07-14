ab7ac43ad880c170ec23d28a6cc7bf9a9d97538c
FanTasyCriT <60607192+FanTasyCriT@users.noreply.github.com>
2021-07-14 10:09:47 +0100

    Delete nicknamegenerator.py

--- nicknamegenerator.py
+++
@@ -1,8 +0,0 @@
-import random
-names = open(r'C:\Users\Fantasy-CriT\Documents\project\male_names.txt', 'r')
-def nicknamegen():
-    content = names.readlines()
-    nickname = content[random.randint(1, len(content))]
-    result = nickname + str(random.randint(10, 9999)) #eliminate \n
-    result = result.replace('\n',"")
-    return result
